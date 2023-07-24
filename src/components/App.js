e22b7b4b2ece4030a640a2ec5a254acfaf454836
pranavbadhan8 <pranavbadhan8@gmail.com>
2023-07-24 09:10:09 +0530

    submitting code-Mon Jul 24 2023 09:10:06 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,38 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+     let[onshow,setOnshow] = useState(false);
+ 
+     function show(){
+      setOnshow(true);
+     }
+
+     function close(){
+      setOnshow(false);
+     }
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="container">
+       <div className="model-overlay">
+         <button onClick={show}>Show Modal</button>
+          
+          { onshow === true && <div className="para">
+            
+              <div className="model-close">
+                <button onClick={close}>Close</button>
+              </div>
+              <div className="model-close">
+                  <p>This is The Content of The Modal</p>
+              </div>
+
+          </div>           
+            }     
+      
+          
+       </div>
     </div>
   )
 }
